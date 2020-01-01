91d45188d857a76433cc206ae6ba0f7417f87ca3
Amir A. Shabani <amirshabani011@gmail.com>
2020-01-01 10:19:36 +0330

    Show number of messages in each file

--- telmex.py
+++ telmex.py
@@ -1,9 +1,22 @@
 import sys, os
+from bs4 import BeautifulSoup
 
-def main(data_path):
-    for file in os.listdir(data_path):
+colors = {
+    "default": "\033[0m",
+    "green": "\033[92m"
+}
+
+def extract(file_path):
+    with open(file_path, 'r') as ofile:
+        soup = BeautifulSoup(ofile.read(), 'html.parser')
+        messages = soup.find_all("div", ["message", "default"])
+        print(f"    {len(messages)} messages")
+
+def main(dir_path):
+    for file in os.listdir(dir_path):
         if(file.endswith(".html")):
-            print(file)
+            print(f"{colors['green']}➤ parsing {file}{colors['default']}")
+            extract(os.path.join(dir_path, file))
 
 if __name__ == "__main__":
     if(len(sys.argv) >= 2):
